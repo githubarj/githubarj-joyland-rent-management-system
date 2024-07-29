c4f5d35e1255a93aae40e4c002b2f03a0d37fbe4
Richard Githuba <githuba9520@gmail.com>
2024-07-29 04:01:12 +0300

    added the invoice to

--- client-joyland/src/views/Invoicing/Components/utils/FormHeader.tsx
+++ client-joyland/src/views/Invoicing/Components/utils/FormHeader.tsx
@@ -1,4 +1,4 @@
-import { Flex, Image, Title } from '@mantine/core';
+import { Flex, Image, Text, Title } from '@mantine/core';
 import React from 'react';
 import freelance from '../../../../../public/assets/freeLance.png';
 import { DatePickerInput } from '@mantine/dates';
@@ -6,9 +6,16 @@ import { DatePickerInput } from '@mantine/dates';
 const FormHeader: React.FC = () => {
   return (
     <Flex className='justify-between'>
-      <div className='flex gap-2'>
-        <Image src={freelance} fit='contain' h={'xl'} />
-        <Title order={2}>Joyland</Title>
+      <div className='flex flex-col gap-2'>
+        <div className='flex gap-2'>
+          <Image src={freelance} fit='contain' h={'xl'} />
+          <Title order={2}>Joyland</Title>
+        </div>
+        <div className='flex flex-col'>
+          <Text>Kasarani, Nairobi</Text>
+          <Text>Paybill : 55543</Text>
+          <Text>Account Number: #house-number</Text>
+        </div>
       </div>
 
       <div>
--- client-joyland/src/views/Invoicing/Components/utils/InvoiceForm.tsx
+++ client-joyland/src/views/Invoicing/Components/utils/InvoiceForm.tsx
@@ -1,14 +1,15 @@
 import { Divider, Flex, Image, Paper } from '@mantine/core';
 import React from 'react';
 import FormHeader from './FormHeader';
+import InvoiceTo from './InvoiceTo';
 
 const InvoiceForm: React.FC = () => {
   return (
     <Paper shadow='xs' className='flex-1  p-4'>
       <form action=''>
         <FormHeader />
-
         <Divider my={'xl'} />
+        <InvoiceTo />
       </form>
     </Paper>
   );
